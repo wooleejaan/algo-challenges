be7fbab1c52dd4f4d65b02daf24696e9843dd15d
Yong-Woo, Lee <110705019+wooleejaan@users.noreply.github.com>
2023-09-05 21:35:58 +0900

    [level 3] Title: 섬 연결하기, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -0,0 +1,16 @@
+function solution(n, costs) {
+    costs.sort((a,b) => a[2] - b[2])
+    let [from, to, answer] = costs.shift()
+    let connected = new Set([from, to])
+    
+    while(connected.size < n){
+        let index = costs.findIndex(([from, to]) => (connected.has(from) && !connected.has(to)) || (connected.has(to) && !connected.has(from)))
+        
+        let [[from, to, cost]] = costs.splice(index, 1)
+        
+        answer += cost 
+        connected.add(from).add(to)
+        
+    }
+    return answer 
+}
