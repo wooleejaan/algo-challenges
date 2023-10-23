260dbc60bbe145c19ea24c6e8c25c6854d6ab729
wooleejaan <wooleejaan@gmail.com>
2023-10-23 13:26:32 +0900

    feat: 오큰수 cpp 풀이 추가

--- cpp-playground/b.cpp
+++ cpp-playground/b.cpp
@@ -1,28 +1,19 @@
 #include<bits/stdc++.h>
 using namespace std;
-vector<int> v[10001];
-int dp[10001], mx, visited[10001], n, m, a, b;
-int dfs(int here){
-    visited[here] = 1;
-    int ret = 1;
-    for(int there : v[here]){
-        if(visited[there]) continue;
-        ret += dfs(there);
-    }
-    return ret;
-}
+int n, a[1000004], ret[1000004];
+stack<int> stk;
 int main(){
     ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    cin >> n >> m;
-    while(m--){
-        cin >> a >> b;
-        v[b].push_back(a);
+    cin >> n;
+    memset(ret, -1, sizeof(ret));
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+        while(stk.size() && a[stk.top()] < a[i]){
+            ret[stk.top()] = a[i];
+            stk.pop();
+        }
+        stk.push(i);
     }
-    for(int i=1; i<=n; i++){
-        memset(visited, 0, sizeof(visited));
-        dp[i] = dfs(i);
-        mx = max(dp[i], mx);
-    }
-    for(int i=1; i<=n; i++) if(mx == dp[i]) cout << i << " ";
+    for(int i=0; i<n; i++) cout << ret[i] << " ";
     return 0;
 }
