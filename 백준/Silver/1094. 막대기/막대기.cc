4cc76fecaa383963b66a374a48e30fc0054ae14a
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2023-11-09 18:53:23 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n, ret = 1;
+int main(){
+    cin >> n;
+    while(n != 1){
+        if(n & 1) ret++;
+        n /= 2;
+    }
+    cout << ret << "\n";
+    return 0;
+}
