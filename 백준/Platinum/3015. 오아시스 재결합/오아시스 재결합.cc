e954be3390dfa078505d088be1abc50d847183ff
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2023-11-12 22:21:39 +0900

    [Platinum V] Title: 오아시스 재결합, Time: 336 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Platinum/3015. 오아시스 재결합/오아시스 재결합.cc
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n, ret, temp;
+stack<pair<ll, ll>> s;
+int main(){
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> temp;
+        int cnt = 1;
+        while(s.size() && s.top().first <= temp){
+            ret += s.top().second;
+            if(s.top().first == temp){
+                cnt = s.top().second + 1;
+            } else {
+                cnt = 1;
+            } 
+            s.pop();
+        }
+        if(s.size()) ret++;
+        s.push({temp, cnt});
+    }
+    cout << ret << "\n";
+    return 0;
+}
