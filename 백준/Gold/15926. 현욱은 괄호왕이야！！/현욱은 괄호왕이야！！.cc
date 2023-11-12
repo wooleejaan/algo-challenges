aa994310f661f26749b22f6918f5531ddf0901ae
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2023-11-12 11:45:05 +0900

    [Gold III] Title: 현욱은 괄호왕이야!!, Time: 4 ms, Memory: 3972 KB -BaekjoonHub

---
+++ 백준/Gold/15926. 현욱은 괄호왕이야！！/현욱은 괄호왕이야！！.cc
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n, d[200001], ret, cnt;
+string s;
+stack<int> stk;
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> n >> s;
+    for(int i=0; i<n; i++){
+        if(s[i] == '(') stk.push(i);
+        else if(stk.size()){
+            d[i] = d[stk.top()] = 1;
+            stk.pop();
+        }
+    }
+    for(int i=0; i<n; i++){
+        if(d[i]) {
+            cnt++;
+            ret = max(ret, cnt);
+        } else cnt = 0;
+    }
+    cout << ret << "\n";
+    return 0;
+}
