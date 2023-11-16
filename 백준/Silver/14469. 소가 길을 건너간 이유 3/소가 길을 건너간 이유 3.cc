49b903f8b8adea2d62b7478fcefa319f25631786
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2023-11-17 00:59:15 +0900

    [Silver IV] Title: 소가 길을 건너간 이유 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/14469. 소가 길을 건너간 이유 3/소가 길을 건너간 이유 3.cc
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int n;
+int main(){
+    cin >> n;
+    vector<pair<int, int>> a(n);
+    for(int i=0; i<n; i++) cin >> a[i].first >> a[i].second;
+    sort(a.begin(), a.end());
+    int realTime = a[0].first + a[0].second;
+    for(int i=1; i<a.size(); i++){
+        realTime = max(realTime, a[i].first);
+        realTime += a[i].second;
+    }
+    cout << realTime << "\n";
+    return 0;
+}
