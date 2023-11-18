450044d5de5d449326a3e761250f38e64482fefd
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2023-11-19 02:05:25 +0900

    [Gold IV] Title: List of Unique Numbers, Time: 16 ms, Memory: 3584 KB -BaekjoonHub

---
+++ 백준/Gold/13144. List of Unique Numbers/List of Unique Numbers.cc
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+long long s, e, cnt[100001], n, a[100001], ret;
+int main(){
+    scanf("%d", &n);
+    for(int i=0; i<n; i++){
+        scanf("%lld", a + i);
+    }
+    while(e < n){
+        if(!cnt[a[e]]){
+            cnt[a[e]]++;
+            e++;
+        } else {
+            ret += (e - s);
+            cnt[a[s]]--;
+            s++;
+        }
+    }
+    ret += (long long)(e - s) * (e - s + 1) / 2; 
+    printf("%lld\n", ret);
+    return 0;
+}
