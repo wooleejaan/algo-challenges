2148a3b45ef47283b6201d6ac29d88aa538cbd31
yongwoo <110705019+wooleejaan@users.noreply.github.com>
2023-12-14 07:29:18 +0900

    [Silver I] Title: 보석 상자, Time: 120 ms, Memory: 4364 KB -BaekjoonHub

---
+++ 백준/Silver/2792. 보석 상자/보석 상자.cc
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll n, m, a[300004], ret = 1e18;
+bool check(ll mid){
+    ll num = 0;
+    for(int i=0; i<m; i++){
+        num += a[i] / mid;
+        if(a[i] % mid) num++;
+    }
+    return n >= num;
+}
+int main(){
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    cin >> n >> m;
+    ll lo = 1, hi = 0, mid;
+    for(int i=0; i<m; i++) cin >> a[i], hi = max(hi, a[i]);
+    while(lo <= hi){
+        mid = (lo + hi) / 2;
+        if(check(mid)){
+            ret = min(ret, mid);
+            hi = mid - 1;
+        } else lo = mid + 1;
+    }
+    cout << ret << "\n";
+    return 0;
+}
